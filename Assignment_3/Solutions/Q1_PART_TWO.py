220bbc9a980a2fab50d106c4941792232cbcc86b
SnehaM <krishnamoorthy.sn@husky.neu.edu>
2017-04-07 17:35:08 -0400

    assignment 3

--- Assignment_3/Solutions/Q1_PART_TWO.py
+++ Assignment_3/Solutions/Q1_PART_TWO.py
@@ -1,7 +1,9 @@
 import pandas as pd
 loc= "/Users/Sneha/Documents/material_for_python/DataAnalysis4Python_Spring17/Assignments/Assignment 3/Data/vehicle_collisions.csv"
 data = pd.read_csv(loc)
+#Picking up required columns
 req_data=data[['BOROUGH','VEHICLE 1 TYPE','VEHICLE 2 TYPE','VEHICLE 3 TYPE','VEHICLE 4 TYPE','VEHICLE 5 TYPE']]
+#Function to count the number of vehicles involved
 def num_of_veh(v1):
     count=0
     if (type(v1[1])==str):
@@ -16,9 +18,12 @@ def num_of_veh(v1):
         count+=1
     return count
 col=['BOROUGH','ONE_VEHICLE_INVOLVED','TWO_VEHICLE_INVOLVED','THREE_VEHICLE_INVOLVED','MORE_VEHICLE_INVOLVED']
+#New dataframe for the output which has the above mentioned columns
 df=pd.DataFrame(columns=col)
+#To add the Borough names to output dataframe
 def trans(a):
     df.loc[len(df)]=[a[0],'0','0','0','0']
+#To add the counts to the ouput df
 def add_val(s):
     if s[1]==1:
         df.loc[df.BOROUGH==s[0],'ONE_VEHICLE_INVOLVED']=s[2]
@@ -28,11 +33,14 @@ def add_val(s):
         df.loc[df.BOROUGH==s[0],'THREE_VEHICLE_INVOLVED']=s[2]
     if s[1]==4:
         df.loc[df.BOROUGH==s[0],'MORE_VEHICLE_INVOLVED']=s[2]
-
+#Calculating the number of vehicles involved in the accident
 req_data['Vehicles_involved']=req_data.apply(num_of_veh,axis=1)
+#groupby based on number of vehicles for each borough
 r1=req_data.groupby(['BOROUGH','Vehicles_involved']).size().reset_index(name='Count')
+#Aggregating 0,4 and 5 vehicles as a single count
 r1.loc[:, 'Vehicles_involved'].replace([0, 5], [4, 4], inplace=True)
 r2=r1.groupby(['BOROUGH','Vehicles_involved']).sum().reset_index()
+#Applying fucntion to add all bororugh names to the df and taking distinct ones
 r2.apply(trans,axis=1)
 df.drop_duplicates('BOROUGH', inplace=True)
 r2.apply(add_val,axis=1)
--- Assignment_3/Solutions/Q4_PART_ONE.py
+++ Assignment_3/Solutions/Q4_PART_ONE.py
@@ -1,7 +1,10 @@
 import pandas as pd
 loc= "/Users/Sneha/Documents/python/Python_assignments/Assignment_3/Data/movies_awards.csv"
 data = pd.read_csv(loc)
+#Dropping all NAN values
 var=data[['Title','Awards']].dropna()
+
+#Creating all the columns in the output
 var['Awards_won']=0
 var['Awards_nominated']=0
 var['Prime_Awards_nominated']=0
@@ -13,9 +16,9 @@ var['Oscar_Awards_won']=0
 var['Golden_Globe_won']=0
 var['BAFTA_won']=0
 global column
-#var['val']=var['Awards'].map(lambda x: x.split())
-temp_val=0
 
+temp_val=0
+#Function for picking up the number of wins of record
 def win(c):
     global temp_val
     if 'win' in c:
@@ -32,6 +35,7 @@ def win(c):
     else:
         return 0
 
+#Function for picking up the number of nominations of record
 def nom(c):
         global temp_val
         if 'nomination' in c:
@@ -48,6 +52,7 @@ def nom(c):
         else:
             return 0
 
+#Function to count the number of awards won for respective award
 def won_award(c):
     if "Won" in c and "Golden" in c:
         va = c.split()
@@ -64,6 +69,7 @@ def won_award(c):
     else:
         return 0
 
+#Function to count the number of awards nominated for respective award
 def nom_award(c):
     if "Nominated" in c and "Golden" in c:
         va = c.split()
@@ -79,9 +85,12 @@ def nom_award(c):
         return int(va[2])
     else:
         return 0
+
+#Calling functions to obtain win and nomination counts
 var['Awards_won']= var['Awards'].map(win)
 var['Awards_nominated']= var['Awards'].map(nom)
 
+#Calling functions to obtain win and nomination counts for the respective awards
 var['Golden_Globe_won']=var['Awards'].map(lambda x: won_award(x) if "Golden" in x else 0 )
 var['Oscar_Awards_won']=var['Awards'].map(lambda x: won_award(x) if "Oscar" in x else 0)
 var['Prime_Awards_won']=var['Awards'].map(lambda x: won_award(x)if "Primetime" in x else 0)
@@ -92,6 +101,7 @@ var['Oscar_Awards_nominated']=var['Awards'].map(lambda x: nom_award(x) if "Oscar
 var['Prime_Awards_nominated']=var['Awards'].map(lambda x: nom_award(x)if "Primetime" in x else 0)
 var['BAFTA_nominated']=var['Awards'].map(lambda x: nom_award(x)if "BAFTA" in x else 0)
 
+#Calculating Total of all awards and nominations
 var['Total']= var.sum(axis=1)
 
 print(var.head())
